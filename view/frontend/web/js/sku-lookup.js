8ebf3255538509f9a142ad8d162e64217482cbec
Mark Shust <mark@shust.com>
2021-07-30 13:36:40 -0400

    Handle Ajax success & error responses in Knockout.js

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -12,7 +12,8 @@ define([
     return Component.extend({
         defaults: {
             sku: ko.observable('24-MB01'),
-            placeholder: 'Example: 24-MB01'
+            placeholder: 'Example: 24-MB01',
+            messageResponse: ko.observable('')
         },
         initialize() {
             this._super();
@@ -20,9 +21,14 @@ define([
             console.log('The skuLookup component has been loaded.');
         },
         handleSubmit() {
+            this.messageResponse('');
+
             storage.get(`rest/V1/products/${this.sku()}`)
                 .done(response => {
-                    console.log(response);
+                    this.messageResponse(`Product found! <strong>${response.name}</strong>`);
+                })
+                .fail(() => {
+                    this.messageResponse('Product not found.');
                 })
         }
     });
