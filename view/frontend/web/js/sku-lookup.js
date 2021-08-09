4cb9511fc5e077e7928e47fb063f07907afd06c2
Mark Shust <mark@shust.com>
2021-08-09 17:10:17 -0400

    Use a Knockout.js style data binding attribute for reactive inline styling

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -13,7 +13,8 @@ define([
         defaults: {
             sku: ko.observable('24-MB01'),
             placeholder: 'Example: 24-MB01',
-            messageResponse: ko.observable('')
+            messageResponse: ko.observable(''),
+            isSuccess: ko.observable(false)
         },
         initialize() {
             this._super();
@@ -22,13 +23,16 @@ define([
         },
         handleSubmit() {
             this.messageResponse('');
+            this.isSuccess(false);
 
             storage.get(`rest/V1/products/${this.sku()}`)
                 .done(response => {
                     this.messageResponse(`Product found! <strong>${response.name}</strong>`);
+                    this.isSuccess(true);
                 })
                 .fail(() => {
                     this.messageResponse('Product not found.');
+                    this.isSuccess(false);
                 })
         }
     });
