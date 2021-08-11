e1e90689750f5e008afb6844bda0ee60a117f750
Mark Shust <mark@shust.com>
2021-08-11 15:00:49 -0400

    Translate text in Magento Knockout.js JavaScript files

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -2,19 +2,21 @@ define([
     'uiComponent',
     'ko',
     'mage/storage',
-    'jquery'
+    'jquery',
+    'mage/translate'
 ], function(
     Component,
     ko,
     storage,
-    $
+    $,
+    $t
 ) {
     'use strict';
 
     return Component.extend({
         defaults: {
             sku: ko.observable('24-MB01'),
-            placeholder: 'Example: 24-MB01',
+            placeholder: $t('Example: %1').replace('%1', '24-MB01'),
             messageResponse: ko.observable(''),
             isSuccess: ko.observable(false)
         },
@@ -30,11 +32,11 @@ define([
 
             storage.get(`rest/V1/products/${this.sku()}`)
                 .done(response => {
-                    this.messageResponse(`Product found! <strong>${response.name}</strong>`);
+                    this.messageResponse($t('Product found! %1').replace('%1', `<strong>${response.name}</strong>`));
                     this.isSuccess(true);
                 })
                 .fail(() => {
-                    this.messageResponse('Product not found.');
+                    this.messageResponse($t('Product not found.'));
                     this.isSuccess(false);
                 })
                 .always(() => {
