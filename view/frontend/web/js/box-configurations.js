13ebcf42544f96d0987d2652c5e834d6acc2f39a
Mark Shust <mark@shust.com>
2021-08-17 21:03:17 -0400

    Build and render multi-dimensional observable arrays in Knockout.js

--- view/frontend/web/js/box-configurations.js
+++ view/frontend/web/js/box-configurations.js
@@ -9,7 +9,15 @@ define([
 
     return Component.extend({
         defaults: {
-            boxConfigurations: ko.observableArray(['abc', 'def'])
+            boxConfigurations: ko.observableArray([{
+                length: 10,
+                width: 12,
+                height: 14
+            }, {
+                length: 13,
+                width: 15,
+                height: 17
+            }])
         },
         initialize() {
             this._super();
