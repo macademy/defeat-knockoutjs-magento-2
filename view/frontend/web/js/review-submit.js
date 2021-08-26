8ec0cba2eae000264887793ca163e880c37ab40f
Mark Shust <mark@shust.com>
2021-08-26 19:52:24 -0400

    Challenge Review: Create a UI Component,  Knockout.js template and model

---
+++ view/frontend/web/js/review-submit.js
@@ -0,0 +1,40 @@
+define([
+    'uiComponent',
+    'ko',
+    'Macademy_InventoryFulfillment/js/model/sku',
+    'Macademy_InventoryFulfillment/js/model/box-configurations'
+], function(
+    Component,
+    ko,
+    skuModel,
+    boxConfigurationsModel
+) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            numberOfBoxes: 0,
+            shipmentWeight: 0,
+            billableWeight: 0,
+            isTermsChecked: ko.observable(false)
+        },
+        initialize() {
+            this._super();
+
+            console.log('The reviewSubmit component has been loaded.');
+
+            this.canSubmit = ko.computed(() => {
+                return skuModel.isSuccess()
+                    && boxConfigurationsModel.isSuccess()
+                    && this.isTermsChecked();
+            })
+        },
+        handleSubmit() {
+            if (this.canSubmit()) {
+                console.log('The Review Submit form has been submitted.');
+            } else {
+                console.log('The Review Submit form has an error.');
+            }
+        }
+    });
+});
